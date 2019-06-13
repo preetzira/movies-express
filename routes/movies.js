847168bdceaf0cfd497be6f0ed6f545884e603a6
Gurpreet Singh <preetzira08@gmail.com>
2019-06-13 14:01:44 +0530

    pagination added and movies api changed

--- routes/movies.js
+++ routes/movies.js
@@ -3,21 +3,21 @@ let router = express.Router();
 const MoviesDAO = require('../DAO/moviesDao');
 
 /* GET users listing. */
-router.post('/', async (req, res, next) => {
-  const { query, search, page } = req.body;
-  MoviesDAO.getMovies(req.body).then(data=>{
-  	if(page>=1){
-  		if(data.length){
-  			return res.status(200).json({status:200,data:data,request:req.body})
-  		} else{
-  			return res.send({status:404,message:"no more movies found"})
-  		}
-  	}
+router.get('/', async (req, res, next) => {
+  const { query, search, page } = req.query;
+  MoviesDAO.getMovies(req.query).then(data=>{
+  	// if(page>=1){
+  	// 	if(data.length){
+  	// 		return res.status(200).json({status:200,data:data,request:req.query})
+  	// 	} else{
+  	// 		return res.send({status:404,message:"no more movies found"})
+  	// 	}
+  	// }
   	const responseData = {
   		data,
-  		title:`Movies Express - ${req.body.query}`,
-  		request : req.body
-  		}
+  		title:`Movies Express - ${req.query.query}`,
+  		request : { "query": query, "search":search, "page":page }
+  	}
   	res.render('movies',responseData);
   }).catch((e)=>{
   	console.error(e.stack)
